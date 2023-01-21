ef3808d18669e8e8e5f70fd485a9feb7404720d1
Kolyaup <123232256+Mirolek13@users.noreply.github.com>
2023-01-21 09:42:59 +0000

    Start code

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Hello")
-print("Hello World")
+from random import randint
+class Student:
+  def characteristicsPeople(self, name):
+    self.name = "Dima"
+    self.study = 0
+    self.happy = 100
+    self.money = 5
+  def studyPeople(self):
+    self.study += 10
+    self.happy -= 20
+    self.money += 20
+  def happyPeople(self):
+    self.happy += 20
+    self.study -= 15
+    self.money -= 10
+  def sleepPeople(self):
+    self.happy += 10
+  def shop(self):
+    if self.money >= 20:
+      self.money -= 20
